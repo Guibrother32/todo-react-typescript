4e9673dc3e98581022bfdba9727314d97b54d718
guilherme4i <guilherme.oliveira@4intelligence.com.br>
2020-10-29 15:33:59 -0300

    Test implemented with JEST

---
+++ src/tests/Test.spec.ts
@@ -0,0 +1,26 @@
+// import { render } from 'react-testing-library';
+import { idText } from 'typescript';
+import App from '../App';
+import ReactDOM from 'react-dom';
+
+// describe('is working', () => {
+//   describe('toggleTodo', () => {
+//     it('should render without crashing', =>{
+//       const div = document.createElement('div');
+//       ReactDOM.render(<App><App/> , div);
+//     });
+//   });
+//   it('should work', () => {
+//     expect(true).toBeTruthy();
+//   });
+// });
+
+// it('renders button correctly', () =>{
+//   render(<App/>)
+// })
+
+describe('is working', () => {
+  it('should work', () => {
+    expect(true).toBeTruthy();
+  });
+});
